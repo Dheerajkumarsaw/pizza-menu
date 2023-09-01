5f700cf39efd5dab5a433a47a8ba05dc80abb118
Dheeraj <dheerajkumarsaw99@gmail.com>
2023-09-01 08:25:21 +0530

    pizza menu add

---
+++ src/data.js
@@ -0,0 +1,53 @@
+const pizzaData = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "pizzas/focaccia.jpg",
+      quantity: 10,
+      soldOut: false
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato, cheese and mozarella",
+      price: 10,
+      photoName: "pizzas/margherita.jpg",
+      quantity: 10,
+      soldOut: false
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName: "pizzas/spinaci.jpg",
+      quantity: 10,
+      soldOut: false
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "pizzas/funghi.jpg",
+      quantity: 10,
+      soldOut: false
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "pizzas/salamino.jpg",
+      quantity: 10,
+      soldOut: false
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "pizzas/prosciutto.jpg",
+      quantity: 10,
+      soldOut: false
+    }
+  ];
+  
+  export default pizzaData;
+  
